3622240933e2f52ce08675db205084193d9be68b
Arie Blumenzweig <blumzi@013.net>
2022-01-08 18:59:05 +0200

    wip

--- sections/software.sh
+++ sections/software.sh
@@ -1,7 +1,5 @@
 #!/bin/bash
 
-user_last="ocs"
-
 sections_register_section "software" "Manages our own LAST software" "user"
 
 function software_enforce() {
@@ -12,14 +10,18 @@ function software_check() {
     su "${user_last}" -c "fetch-from-github --dir ~ --check"
 }
 
-function software_policy() {
+function software_policy() { 
     cat <<- EOF
 
     All the LAST computers are both production AND development machines.  As such they
      contain git clones of the relevant software repositories (on github). You
 
     - $(ansi_underline "${PROG} check software") - checks if the local sources are up-to-date
-    - $(ansi_underline "${PROG} enforce software") - pulls the lates sources from the repositories
+    - $(ansi_underline "${PROG} enforce software") - pulls the latest sources from the repositories
     
+    Software repsitories:
+
 EOF
+    su "${user_last}" -c "fetch-from-github --dir ~ --list"
+    echo ""
 }
