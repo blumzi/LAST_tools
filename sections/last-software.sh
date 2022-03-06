d687cef2e11fdedfb78b7cf710c49e21d2b3ee69
Arie Blumenzweig <theblumz@gmail.com>
2022-03-06 13:41:34 +0000

    Added missing flag

--- sections/last-software.sh
+++ sections/last-software.sh
@@ -53,7 +53,7 @@ function last_software_enforce() {
             message_success "The CME2 desktop shortcut exists"
         else
             cp "$(module_locate files/CME2.desktop)" "${shortcut}"
-            desktop-file-install "${shortcut}"
+            desktop-file-install --rebuild-mime-info-cache "${shortcut}"
             message_success "Installed the CME2 desktop shortcut"
         fi
     fi
