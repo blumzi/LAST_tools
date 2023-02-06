4da61ed7cebf2b85a3565f9b64a53bc6ad8adea6
blumzi <theblumz@gmail.com>
2023-02-06 13:15:13 +0000

    discarded unneeded output

--- sections/ssh.sh
+++ sections/ssh.sh
@@ -171,7 +171,7 @@ function ssh_check_keys() {
         (( errors++ ))
     fi
 
-    su - ocs -c 'ssh-keyscan -H localhost >> ~/.ssh/known_hosts'
+    su - ocs -c '2>/dev/null ssh-keyscan -H localhost >> ~/.ssh/known_hosts'
 
     local answer status
     answer=$(timeout 2s su "${user_name}" -c 'ssh localhost id -u')
