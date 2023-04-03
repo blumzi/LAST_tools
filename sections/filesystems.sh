1ebc7a92c5a3172e1e167f8dd012d82dc830c5d1
blumzi <theblumz@gmail.com>
2023-04-03 15:10:10 +0000

    some minor fixes

--- sections/filesystems.sh
+++ sections/filesystems.sh
@@ -154,6 +154,7 @@ function filesystems_enforce() {
     if ! ${_filesystems_last0}; then
         tmp=$(mktemp)
         config_file="/etc/auto.master.d/last0.autofs"
+        mkdir -p $(dirname ${config_file})
         {
             grep -v last0 "${config_file}" 2> /dev/null
             echo "/last0 /etc/auto.last0"
@@ -170,8 +171,6 @@ function filesystems_enforce() {
         mkdir -p /last0
         systemctl start autofs
     fi
-
-    filesystems_enforce_
 }
 
 function filesystems_check() {
