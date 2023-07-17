51d8554c5f8cee00f130dd26eb4ef8ade2c14cb8
The LAST Ocs <>
2023-07-17 11:43:48 +0000

    renamed last-rsync-xxx to last-products-xxx, now handling DB insertion as well

---
+++ lib/const.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+#
+# Some constants used troughout this project
+#
+
+#
+# These are added by the pipeline to a .status file
+#  to signal that they are stable and can be processed
+#
+const_ready_for_transfer="ready-for-transfer"
+const_transfered="transfered"
+
+const_ready_for_db="ready-for-DB"
+const_inserted="inserted"
