e505040824ba5f072b1508fe119af52998a66132
The LAST Ocs <>
2024-02-28 14:19:59 +0000

    new(er) policy for insertions into the database

--- lib/postgres.sh
+++ lib/postgres.sh
@@ -11,3 +11,6 @@ postgres_port="5432"
 
 postgres_proc_catalog_table="proc_src_catalog"
 postgres_coadd_catalog_table="coadd_src_catalog"
+postgres_raw_image_table="raw_images"
+postgres_proc_image_table="proc_images"
+postgres_coadd_image_table="coadd_images"
