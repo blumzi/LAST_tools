4fef91e79395c6d91bdc84b9c1b20e39b772822c
The LAST Ocs <>
2024-05-06 08:00:13 +0000

    fixed the check routine

--- sections/crontab.sh
+++ sections/crontab.sh
@@ -13,27 +13,30 @@ function crontab_enforce() {
             echo "0  7 * * * /usr/local/share/last-tool/bin/last-pipeline-digest"
         fi
         echo "0  8 * * * /usr/local/share/last-tool/bin/last-backup --all --remove --force"
-        echo "0 12 * * * /usr/local/share/last-tool/bin/last-compress-raw-images"
+        echo "#0 12 * * * /usr/local/share/last-tool/bin/last-compress-raw-images"
     } | crontab -u ${user_name} -
     message_success "crontab: Updated crontab for user \"${user_name}\""
 }
 
 function crontab_check() {
     local expected found pattern ret
+    local -a entries with_commas
 
     pattern='last-(backup|compress-raw-images|pipeline-digest)'
+    entries=( last-backup last-compress-raw-images )
     if macmap_this_is_last0; then
-        expected=3
-    else
-        expected=2
+        entries+=( last-pipeline-digest )
     fi
+    pattern=$( echo "${entries[@]}" | tr ' ' '|' )
+    expected=${#entries[@]}
+    with_commas=$( echo "${entries[@]}" | tr ' ' ',' )
 
     ret=0
-    found=$( crontab -l | egrep  --count 'last-(backup|compress-raw-images)' )
+    found=$( crontab -l -u ${user_name} | egrep  --count "${pattern}" )
     if (( found == expected )); then
-        message_success "crontab: found ${expected} crontab lines"
+        message_success "crontab: found expected lines for ${with_commas}"
     else
-        message_failure "crontab: found only ${found} lines (expected: ${expected})"
+        message_failure "crontab: only ${found} out of ${expected} lines for ${with_commas} were found"
         (( ret++ ))
     fi
 
